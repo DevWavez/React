5a01dbb180121ba455352d3ed45c0a2045d26241
DevWavez <devwave@outlook.de>
2023-10-30 19:04:11 +0100

    change position

--- pixagram/src/components/Bottombar.jsx
+++ pixagram/src/components/Bottombar.jsx
@@ -1,25 +1,35 @@
 const Bottombar = () => {
   return (
-    <div className="fixed left-0 top-[5rem] w-[80px] h-screen flex justify-between items-center px-4 bg-black text-gray-300">
+    <div className="fixed left-0 bottom-0 w-full h-[80px] flex justify-between items-center px-4 bg-black text-gray-300">
       {/* Sidebar Desktop */}
 
       {/* Bottem Bar mobile */}
       <div>
         <ul className="flex flex-row">
-          <li className="ml-2 ">
-            <a href="/">Home</a>
+          <li className="ml-2">
+            <a href="/">
+              <i className="icon-home"></i> Home
+            </a>
           </li>
           <li className="ml-2">
-            <a href="/">Explore</a>
+            <a href="/">
+              <i className="icon-search"></i> Explore
+            </a>
           </li>
           <li className="ml-2">
-            <a href="/">People</a>
+            <a href="/">
+              <i className="icon-users"></i> People
+            </a>
           </li>
           <li className="ml-2">
-            <a href="/">Saved</a>
+            <a href="/">
+              <i className="icon-heart"></i> Saved
+            </a>
           </li>
           <li className="ml-2">
-            <a href="/">Ranking</a>
+            <a href="/">
+              <i className="icon-chart"></i> Ranking
+            </a>
           </li>
         </ul>
       </div>
